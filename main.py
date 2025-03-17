fc584615a2b616c955d6f02a51cd1620bb6d859e
Reshmanjali12 <maddulareshmanjali@gmail.com>
2025-03-18 00:40:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,51 +3,63 @@ import time
 from food import Food
 from Snake import Snake
 from scoreboard import Score
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.title("My Snake Game")
-screen.bgcolor("black")
-screen.tracer(0)
-snake = Snake()
-food = Food()
-score = Score()
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.left, "Left")
 
-is_game_on = True
-while is_game_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-    # collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        score.increase_score()
-    # collision with wall
-    if snake.head.xcor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
-        score.reset()
-        data = str(score.high_score)
-        with open("data.txt", mode="w") as file:
-            file.write(data)
-        snake.reset()
-    # collision with wall
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+def main():
+    # Setup screen
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.title("My Snake Game")
+    screen.bgcolor("black")
+    screen.tracer(0)
+
+    # Create game objects
+    snake = Snake()
+    food = Food()
+    score = Score()
+
+    # Listen for keyboard input
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.left, "Left")
+
+    # Game loop
+    is_game_on = True
+    while is_game_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        # Collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            score.increase_score()
+
+        # Collision with wall
+        if (
+            snake.head.xcor() < -280 or snake.head.xcor() > 280 or
+            snake.head.ycor() < -280 or snake.head.ycor() > 280
+        ):
             score.reset()
-            data = str(score.high_score)
-            with open("data.txt", mode="w") as file:
-                file.write(data)
+            save_high_score(score.high_score)
             snake.reset()
 
+        # Collision with self
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                score.reset()
+                save_high_score(score.high_score)
+                snake.reset()
 
+    screen.exitonclick()
 
+# Function to save high score to a file
+def save_high_score(high_score):
+    with open("data.txt", mode="w") as file:
+        file.write(str(high_score))
 
-
-
-
-
-screen.exitonclick()
+# Run the game
+if __name__ == "__main__":
+    main()
